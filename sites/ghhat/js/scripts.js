0365c48758526a515144150eeeacfd7014f0c5cd
Steve Medley <steve@atlantisitprofessionals.com>
2016-08-08 00:19:30 -0700

    Match prject

--- sites/ghhat/js/scripts.js
+++ sites/ghhat/js/scripts.js
@@ -9,9 +9,27 @@ var spotNo11 = "<iframe src='https://embed.spotify.com/?uri=spotify:track:2sDEvu
 var spotNo15 = "<iframe class='right' src='https://embed.spotify.com/?uri=spotify:track:6WNL5LE1w2JmhGES04KhRQ' width='300' height='80' frameborder='0' allowtransparency='true'></iframe>"
 
 /*var spotSongs = {
-	"title": [],
-	"intro": [],
-	"embed": []
+	"title": [
+		"Joyogistic",
+		"Primal",
+		"Piano Jam",
+		"Act II, No. 11",
+		"Act II, No. 15"
+	],
+	"intro": [
+		"This is a little blurb about the song",
+		"This is a little blurb about the song",
+		"This is a little blurb about the song",
+		"This is a little blurb about the song",
+		"This is a little blurb about the song"
+	],
+	"embed": [
+		"<iframe class='right' src='https://embed.spotify.com/?uri=spotify:track:0h6ThT6IIsLBZS3q2xgqHk' width='300' height='80' frameborder='0' allowtransparency='true'></iframe>",
+		"<iframe src='https://embed.spotify.com/?uri=spotify:track:5chJzL7y04dG1tCrG9Bbes' width='300' height='80' frameborder='0' allowtransparency='true'></iframe>",
+		"<iframe class='right' src='https://embed.spotify.com/?uri=spotify:track:6sLi07oCNv1IZsnz3o1eTs' width='300' height='80' frameborder='0' allowtransparency='true'></iframe>",
+		"<iframe src='https://embed.spotify.com/?uri=spotify:track:2sDEvu1S4q4ExhbJVygBz2' width='300' height='80' frameborder='0' allowtransparency='true'></iframe>",
+		"<iframe class='right' src='https://embed.spotify.com/?uri=spotify:track:6WNL5LE1w2JmhGES04KhRQ' width='300' height='80' frameborder='0' allowtransparency='true'></iframe>"
+	]
 }*/
 
 
@@ -30,5 +48,6 @@ function insertSpotify() {
 	insertNeededHTML('song4', spotNo11);
 	insertNeededHTML('song5', spotNo15);
 }
+
 
 $( document ).ready(insertSpotify())
